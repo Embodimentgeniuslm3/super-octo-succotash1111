b3634173acebea93b32fd2a1f0a2cf2ff4194f7f
https://outlook.live.com/mail/0/inbox/id/AQQkADAwATNiZmYAZC0yMTU1AC0wODVhLTAwAi0wMAoAEAAoiDgg4nl1TJ2H8Hlh2a1H <78320452+imgod123456@users.noreply.github.com>
2022-04-07 07:27:53 -0700

    Initialize develop

---
+++ .github/deploy-readme.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+if [ $CIRCLE_BRANCH != "master" ]
+then
+  exit
+fi
+
+git checkout master
+
+remote=$(git config remote.origin.url)
+
+git config user.name "READMEbot"
+git config user.email "readmebot@users.noreply.github.com"
+git config --global user.password $GH_TOKEN > /dev/null 2>&1
+
+echo add readme
+git add README.md database.json
+
+echo commit
+git commit -m "[auto] [ci skip] Generate README & Database.json"
+
+echo push
+git push --quiet --force origin master > /dev/null 2>&1
